5c1eedb8b83cf516ba9277bed819118f2279e423
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-03-14 13:22:55 +0100

    Added CORS and helmet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot()],
     controllers: [AppController],
     providers: [],
 })
