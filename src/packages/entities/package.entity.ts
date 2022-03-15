423574c81b2677b14e1205b28ae24e2c7ed1a170
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-03-15 10:06:55 +0100

    Generate package resources and package schema and model

---
+++ src/packages/entities/package.entity.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Document } from 'mongoose';
+
+export type PackageDocument = Package & Document;
+
+@Schema()
+export class Package {
+    @Prop({
+        required: true,
+    })
+    name: string;
+
+    @Prop({
+        required: true,
+    })
+    price: number;
+
+    @Prop({
+        required: true,
+    })
+    days: number;
+
+    @Prop({
+        required: true,
+    })
+    description: string;
+
+    @Prop({
+        required: true,
+    })
+    icon: string;
+
+    @Prop({
+        ref: 'Surfcamp',
+    })
+    surfcampId: mongoose.Types.ObjectId;
+}
+
+export const PackageSchema = SchemaFactory.createForClass(Package);
+
+PackageSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.__v;
+    },
+});
